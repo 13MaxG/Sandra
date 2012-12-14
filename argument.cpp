1967f8ed9b0a32bdf182157fed86c8b9f9d76eb5
WiecznyWem <WiecznyWem@gmail.com>
2012-12-14 16:20:22 +0100

    Poprawka dla szablonu int dla Argument

--- argument.cpp
+++ argument.cpp
@@ -3,6 +3,7 @@
 
 #include "argument.h"
 #include <iostream>
+#include <cmath>
 
 template <typename T>
 Argument<T>::Argument()
@@ -62,6 +63,56 @@ void Argument<T>::Update(double up)
 
     // Aktualna wartość
     now.Value  = per * ((*next).Value - (*prev).Value) + (*prev).Value;
+
+    // Gotowe
+    current = now;
+}
+
+/**
+ *  W przypadku szablonu int, zwracana zawartość będzie zaokrąglana roundem, a nie dzielona poprostu divem
+ */
+template<>
+inline void Argument<int>::Update(double up)
+{
+
+    Key<int> now; // Nowa wartość zmiennej
+
+    now.Time = current.Time + up; // Wylicz nowy czas
+
+    // Złap takie elementy kluczowe aby obejmowały aktualny czas
+    // (zwykle wystarczy jeden obieg)
+    while(now.Time >= (*next).Time)
+    {
+        // Skocz do elementów dalszch o jeden
+        next++;
+        prev++;
+
+        // (zabezpieczenie)
+        // Jeżeli element następny uderzy w koniec listy
+        // to element poprzedni i następny to ostatni element
+        if((next) == data.end())
+        {
+            next--;
+            prev--;
+            break;//broke; broken :)
+        }
+    }
+
+    //(zabezpieczenie)
+    // jeżeli jest tylko jeden element
+    if(next == data.begin())
+    {
+        prev = next;
+    }
+
+    double per; // Procent przejścia pomiędzy wartościami kluczowymi
+    per = (now.Time-(*prev).Time) / ((*next).Time - (*prev).Time);
+
+    //(zabezpieczenie) zadbaj o przypadki graniczne
+    if(per > 1.0) per = 1.0;
+
+    // Aktualna wartość
+    now.Value  = round( per * ((*next).Value - (*prev).Value) + (*prev).Value );
 
     // Gotowe
     current = now;
